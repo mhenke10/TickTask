06646f8dd0875d227c6d9f4901a5af8acd6e51dd
Mike <mhenke10@gmail.com>
2016-01-28 15:22:54 -0600

    Initial Commit

---
+++ TickTask WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TickTask WatchKit Extension
+//
+//  Created by Michael Henke on 1/28/16.
+//  Copyright © 2016 RJICompetition. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
