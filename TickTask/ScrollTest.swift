2da6959dd228e998884dd9672097e80887165e2e
Thenry945 <travis@henrichs.cc>
2016-01-30 14:00:23 -0600

    Start setup of create task view/controller

---
+++ TickTask/ScrollTest.swift
@@ -0,0 +1,11 @@
+//
+//  ScrollTest.swift
+//  TickTask
+//
+//  Created by Oberd on 1/30/16.
+//  Copyright © 2016 RJICompetition. All rights reserved.
+//
+
+import Foundation
+
+
--- TickTask/ViewController.swift
+++ TickTask/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         print("creating repo")
     }
 
@@ -22,5 +23,7 @@ class ViewController: UIViewController {
     }
 
 
+    
+    
 }
 
