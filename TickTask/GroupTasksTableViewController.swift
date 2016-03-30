2f76521309b027f3e0b2391330f5a15e49f591c6
Thenry945 <travis@henrichs.cc>
2016-03-30 13:26:49 -0500

    adding better tab navigation

---
+++ TickTask/GroupTasksTableViewController.swift
@@ -0,0 +1,125 @@
+//
+//  GroupTasksTableViewController.swift
+//  TickTask
+//
+//  Created by Oberd on 3/30/16.
+//  Copyright © 2016 RJICompetition. All rights reserved.
+//
+
+import UIKit
+
+class GroupTasksTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.automaticallyAdjustsScrollViewInsets = true
+        self.title = "Group Task List"
+        //setting up editing ability
+        navigationItem.leftBarButtonItem = editButtonItem()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //number of sections in table
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //Number or rows in section is equal to count of tasks in taskManager array
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TaskManager.tasks.count
+    }
+    
+    //Fill cells in tableView to be of type task.
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)
+        -> UITableViewCell {
+            let cell = tableView.dequeueReusableCellWithIdentifier("GroupTaskCell", forIndexPath: indexPath) as! TaskCell
+            
+            let task = TaskManager.tasks[indexPath.row] as Task
+            cell.task = task
+            return cell
+    }
+    
+    
+    //segues and handling saving/canceling
+    @IBAction func cancelToTaskListViewController(segue:UIStoryboardSegue) {
+    }
+    
+    @IBAction func saveTask(segue:UIStoryboardSegue) {
+        
+        let indexPath = NSIndexPath(forRow: TaskManager.tasks.count-1, inSection: 0)
+        tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        tableView.reloadData()
+        
+    }
+    
+    //Delete task from tableView / from array of tasks
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            TaskManager.tasks.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
