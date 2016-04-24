e99d666c83c805d0a8206e3d4b4edd834775fd26
Michael Henke <mhenke10@gmail.com>
2016-04-24 16:24:23 -0500

    Task Location additions

---
+++ TickTask/TaskLocationViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TaskLocationViewController.swift
+//  TickTask
+//
+//  Created by Michael Henke on 4/21/16.
+//  Copyright © 2016 RJICompetition. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+
+let kSavedTasksKey = "savedTasks"
+
+class TaskLocationViewController: UITableViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        self.title = "Add Location"
+        // 1
+        locationManager.delegate = self
+        // 2
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    
+    
+    
+    
+}
